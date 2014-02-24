2f7de5e179dde4257c0d2ed72c636adf1af75996
Jeremiah Sweeney <jeremiah.sweeney@hotmail.com>
2014-02-24 16:07:41 -0500

    added comment

--- image_uploader.py
+++ image_uploader.py
@@ -3,39 +3,38 @@ import base64
 import json
 
 
+# Imgur API
 client_id = 'effd0a07a1436c4'
 upload_url = 'https://api.imgur.com/3/image'
 
 
-def getPath():
-    print('Enter the file path of the image to be uploaded:')
-    path = input()
+def get_path():
+    path = input('Enter the file path of the image to be uploaded: ')
     return path
 
-def getImage(path):
+def get_image(path):
     f = open(path, 'rb')
     image = base64.b64encode(f.read())
     return image
 
-def postImage(image):
+def post_image(image):
     payload = {'image': image}
     headers = {'Authorization': 'Client-ID {0}'.format(client_id)}
     request = requests.post(upload_url, data=payload, headers=headers)
     data = json.loads(request.text)
     return data
 
-def getURL(data):
+def get_URL(data):
     if data['success'] == True:
         url = data['data']['link']
         print('Your image is located at:\n%s' % (url))
     else:
         print('Upload failed.')
 
-def main():
-    path = getPath()
-    image = getImage(path)
-    data = postImage(image)
-    getURL(data)
+
+path = get_path()
+image = get_image(path)
+data = post_image(image)
+get_URL(data)
 
 
-main()
